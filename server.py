20539f154ede58356e00d25335cef7f00a03e336
顾煜贤 <gu-yx17@mails.tsinghua.edu.cn>
2020-12-12 11:02:42 +0800

    fix server

--- server.py
+++ server.py
@@ -25,86 +25,75 @@ class Server():
     async def run_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as src_socket:
             src_socket.setblocking(False)
+            src_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             src_socket.bind((self.host, self.port))
             src_socket.listen(socket.SOMAXCONN)
 
             print("Listening at {}:{}".format(self.host, self.port))
             while True:
                 src_conn, src_addr = await self.loop.sock_accept(src_socket)
-                print("Receive from {}".format(src_addr))
                 self.loop.create_task(self.handle(src_conn, src_addr))
 
     async def handle(self, src_conn, src_addr):
-        # 1. Method Negotiation
         src_ip, _ = src_addr
 
         data = await self.loop.sock_recv(src_conn, BUFFER_SIZE)
-    
+
         hit = False
 
         if len(data) >= 4:
             h = data[:4]
-            print("h", h)
-            print("tuple", self.hash.hash_tuple(self.passwd))
             if h in self.hash.hash_tuple(self.passwd):
-                print("h in")
+                print("Hash OK")
                 data = self.rsa_cipher.decode(data[4:])
 
                 if len(data) < 16:
-                    print(" data < 16")
                     src_conn.close()
                     return
 
                 passwd = data[16:]
-                print("passwd", passwd.decode())
                 if passwd != self.passwd:
                     print(src_ip, "wrong passward, close connection")
                     src_conn.close()
                     return
+                
+                print("Password OK")
 
                 hit = True
 
                 R = data[:16]
-                print("R", R)
                 self.ip_key_pool[src_ip] = AESCipher(R)
                 # self.ip_key_pool[src_ip] = EmptyCipher()
 
-                print("sent OK")
                 ok_data = b"OK"
                 ok_data = AESCipher(R).encode(ok_data)
                 await self.loop.sock_sendall(src_conn, ok_data)
 
         if hit:
-            print("hit:", hit)
+            print("Auth OK")
         else:
             if src_ip not in self.ip_key_pool:
-                print("h not in")
+                print("Hash Wrong")
                 src_conn.close()
                 return
             else:
                 aes_cipher = self.ip_key_pool[src_ip] # NOTE get from pool
-    
-                data = data[32:]
-                data = aes_cipher.decode(data)
-                print("Receive Negotiation:", data)
-    
+
+                # 1. Method Negotiation
+                data = self.unpack_decode(data, aes_cipher)
+
                 if not data or data[0] != 0x05:
-                    print("Not socks 5")
                     src_conn.close()
                     return
-    
+
                 # 2. Send Response, select a method. Choose 0x00 for no verification
-                response_data = bytearray((0x05, 0x00))
-                response_data = aes_cipher.encode(response_data)
-                response_data = aes_cipher.encode(int(len(response_data)).to_bytes(16, "little")) + response_data
+                response_data = self.pack_encode(bytearray((0x05, 0x00)), aes_cipher)
                 await self.loop.sock_sendall(src_conn, response_data)
-    
+
                 # 3. Request
                 data = await self.loop.sock_recv(src_conn, BUFFER_SIZE)
-                data = data[32:]
-                data = aes_cipher.decode(data)
-                print("Receive Request:", data)
-    
+                data = self.unpack_decode(data, aes_cipher)
+
                 if len(data) < 7:
                     src_conn.close()
                     return
@@ -113,7 +102,7 @@ class Server():
                     # ensure connect
                     src_conn.close()
                     return
-    
+
                 dst_family = None
                 dst_ip = None
                 dst_port = data[-2:]
@@ -135,10 +124,7 @@ class Server():
                 else:
                     src_conn.close()
                     return
-    
-                print("dst_ip", dst_ip)
-                print("dst_port", dst_port)
-    
+
                 if dst_family is not None:
                     try:
                         dst_socket = socket.socket(
@@ -146,10 +132,10 @@ class Server():
                         dst_socket.setblocking(False)
                         await self.loop.sock_connect(dst_socket, dst_addr)
                     except Exception as e:
-                        print("Caught exception", e)
+                        # print("Caught exception", e)
                         if dst_socket is not None:
                             dst_socket.close()
-                            dstServer = None
+                            dst_socket = None
                 else:
                     for info in await self.loop.getaddrinfo(dst_ip, dst_port):
                         dst_family, socket_type, proto, canonname, dst_addr = info
@@ -159,58 +145,77 @@ class Server():
                             await self.loop.sock_connect(dst_socket, dst_addr)
                             break
                         except Exception as e:
-                            print("Caught exception", e)
+                            # print("Caught exception", e)
                             if dst_socket is not None:
                                 dst_socket.close()
                                 dst_socket = None
-    
+
                 if dst_family is None:
                     return
-    
-                print("send end")
+
                 # 4. End negotiation
-                end_data = bytearray((0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))
-                end_data = aes_cipher.encode(end_data)
-                end_data = aes_cipher.encode(int(len(end_data)).to_bytes(16, "little")) + end_data
+                end_data = self.pack_encode(bytearray((0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)), aes_cipher)
                 await self.loop.sock_sendall(src_conn, end_data)
-    
+
                 src_to_dst_task = self.loop.create_task(self.recv_and_send("src2dst", self.loop, src_conn, dst_socket, True, False, cipher_func=aes_cipher.decode))
                 dst_to_src_task = self.loop.create_task(self.recv_and_send("dst2src", self.loop, dst_socket, src_conn, False, True, cipher_func=aes_cipher.encode))
-    
+
                 task = asyncio.gather(src_to_dst_task, dst_to_src_task, loop=self.loop, return_exceptions=True)
-    
+
                 def clean_up(task):
-                    print("clean up")
+                    # print("clean up")
                     dst_socket.close()
                     src_conn.close()
-    
+
                 task.add_done_callback(clean_up)
+        
+    def unpack_decode(self, data, cipher):
+        data = data[32:]
+        return cipher.decode(data)
+
+    def pack_encode(self, data, cipher):
+        data = cipher.encode(data)
+        return cipher.encode(int(len(data)).to_bytes(16, "little")) + data
 
     async def recv_and_send(self, mode, loop, conn_from, conn_to, unpack_src_length, pack_dst_length, cipher_func):
         while True:
-            data = b""  
-            if unpack_src_length:
-                while len(data) < 32:
-                    data = data + await self.loop.sock_recv(conn_from, 32 - len(data))
-                assert len(data) == 32
-                length = int.from_bytes(cipher_func(data), "little")
-                data = b""
-                while len(data) < length:
-                    data = data + await self.loop.sock_recv(conn_from, min(BUFFER_SIZE, length - len(data)))
-            else:
-                data = await loop.sock_recv(conn_from, BUFFER_SIZE)
-            # print("data", data)
-            if not data:
+            try:
+                data = b""  
+                if unpack_src_length:
+                    running = True
+                    while len(data) < 32 and running:
+                        packet = await self.loop.sock_recv(conn_from, 32 - len(data))
+                        if not packet:
+                            running = False
+                            break
+                        data = data + packet
+                    if not running:
+                        break
+                    assert len(data) == 32
+                    length = int.from_bytes(cipher_func(data), "little")
+                    data = b""
+                    while len(data) < length and running:
+                        packet = await self.loop.sock_recv(conn_from, min(BUFFER_SIZE, length - len(data)))
+                        if not packet:
+                            running = False
+                            break
+                        data = data + packet
+                    if not running:
+                        break
+                else:
+                    data = await loop.sock_recv(conn_from, BUFFER_SIZE)
+                # print("data", data)
+                if not data:
+                    break
+                # print("before", mode, data)
+                data = cipher_func(data)
+                if pack_dst_length:
+                    data = cipher_func(int(len(data)).to_bytes(16, "little")) + data
+                # print("end", mode, data)
+                await loop.sock_sendall(conn_to, data)
+
+            except KeyboardInterrupt:
                 break
-            # print("before", mode, data)
-            data = cipher_func(data)
-            if pack_dst_length:
-                data = cipher_func(int(len(data)).to_bytes(16, "little")) + data
-            # print("end", mode, data)
-            await loop.sock_sendall(conn_to, data)
-
-        print(mode, "close")
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -224,7 +229,7 @@ def main():
     host = "0.0.0.0"
     port = args.port
 
-    print("Start running")
+    print("Remote server starts running")
 
     loop = asyncio.get_event_loop()
 
@@ -232,7 +237,10 @@ def main():
 
     task = loop.create_task(server.run_server())
 
-    loop.run_forever()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("Remote server shuts down")
 
 if __name__ == "__main__":
     main()
